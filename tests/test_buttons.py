ac87f07a801664fcbe83b4895a9cc23ca84829e0
Ben Slater <bas58@cam.ac.uk>
2025-06-08 11:48:36 +0100

    Add a button test

---
+++ tests/test_buttons.py
@@ -0,0 +1,19 @@
+from shared import (
+    run_behaviour_in_aai,
+    forwards_action,
+    nothing_action,
+    backwards_action,
+)
+import os
+
+"""
+Tests that the button works
+"""
+
+
+def test_button_basic():
+    run_behaviour_in_aai(
+        os.path.join(".", "testConfigs", "testButtons.yml"),
+        0.8,
+        lambda r: backwards_action if r < -0.2 else forwards_action,
+    )
