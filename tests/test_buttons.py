c61ad69349842ee635779fba8b59e258cd8023c5
Ben Slater <bas58@cam.ac.uk>
2025-08-25 10:00:11 +0100

    Update button tests for Operations change

--- tests/test_buttons.py
+++ tests/test_buttons.py
@@ -16,4 +16,14 @@ def test_button_basic():
         os.path.join(".", "testConfigs", "testButtons.yml"),
         0.8,
         lambda r: backwards_action if r < -0.2 else forwards_action,
+        watch=True,
+    )
+
+
+def test_button_legacy():
+    run_behaviour_in_aai(
+        os.path.join(".", "testConfigs", "testButtonsLegacy.yml"),
+        0.8,
+        lambda r: backwards_action if r < -0.2 else forwards_action,
+        watch=True,
     )
