0a3254093b962c8e7fde001f1c4897f06b41ba5a
Ben Slater <bas58@cam.ac.uk>
2025-09-05 18:01:30 +0100

    Add a test of the datazone with the spawn object operation

---
+++ tests/test_operations.py
@@ -0,0 +1,17 @@
+from shared import (
+    run_behaviour_in_aai,
+    forwards_action,
+    nothing_action,
+    backwards_action,
+)
+import os
+
+
+# Go through the datazone and collect a reward
+def test_basic_datazone():
+    run_behaviour_in_aai(
+        os.path.join(".", "testConfigs", "testDatazone_spawngoal.yml"),
+        0.8,
+        lambda _: forwards_action,
+        watch=True,
+    )
