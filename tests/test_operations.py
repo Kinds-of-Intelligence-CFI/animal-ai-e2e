2fab8dbe8489bdd9126d5865b2f4abab5065c4d9
Ben Slater <bas58@cam.ac.uk>
2025-09-06 10:38:46 +0100

    Update path in basic datazone operation test
    This was missed in a previous renaming

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -10,8 +10,7 @@ import os
 # Go through the datazone and collect a reward
 def test_basic_datazone():
     run_behaviour_in_aai(
-        os.path.join(".", "testConfigs", "testDatazone_spawngoal.yml"),
+        os.path.join(".", "testConfigs", "testDatazone_spawnobject.yml"),
         0.8,
         lambda _: forwards_action,
-        watch=True,
     )
